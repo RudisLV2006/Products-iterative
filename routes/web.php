dc71cf8c5672a7df3d7fbdb30ee409154795bfcc
DeivisStasuls <deivis.stasuls@gmail.com>
2025-10-31 13:04:02 +0200

    Add product with route, migration, view, controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/product', [ProductController::class, "index"])->name("product.index");
+Route::get('/product/{product}', [ProductController::class, "show"])->name("product.show");
+Route::get('/product/create', [ProductController::class, "create"])->name("product.create");
+Route::post('/product', [ProductController::class, "store"])->name("product.store");
+Route::get('/product/edit', [ProductController::class, "edit"])->name("product.edit");
+Route::put('/product/edit', [ProductController::class, "update"])->name("product.update");
+Route::delete('/product', [ProductController::class, "delete"])->name("product.delete");
