2aea8a678675dd3587d77e4faec879eba6eeba9b
DeivisStasuls <deivis.stasuls@gmail.com>
2025-10-31 13:31:03 +0200

    Remove tailwing; Add css and redact

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Produktu pārvaldība' }}</title>
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 
